01952ad5720501b1d458fff90187e841bdb616a7
Murad Yusfov <muradyusifov969@gmail.com>
2024-09-15 09:39:09 +0400

    hello

---
+++ examples.js
@@ -0,0 +1,82 @@
+document.getElementById("demo").innerHTML
+=5+6;
+
+document.write(5+6);
+
+window.alert()
+// method 2#
+
+
+/*
+<buttom type="buttom"
+onclick="document.write(5+6)">Try it</buttom>
+
+*/
+
+// <script> alert(5+6);</script>
+
+/*
+<script>
+console.log(5 + 6);
+</script>
+*/
+
+
+/* <buttom onclick="window.print()">
+  Print this page
+</buttom>
+
+print method in javascript*/
+
+
+/* 
+  <script>
+    let x, y, z;
+    x=5; 
+    y=6;
+    z=x+y;
+
+    document.getElementById("demo").innerHTML="the value of z is"+z+".";
+  </script>
+note:statemens
+*/
+
+/*
+<script>
+
+document.getElementById("demo").innerHTML="Hello Dolly." ;
+</script>
+
+We can write thecode in the new line
+
+letting you remove text and add new one in the tag*/
+
+
+/*
+
+
+<button type="button" onclick="myFunction()">Click Me!</button>
+
+<p id="demo1"></p>
+<p id="demo2"></p>
+
+<script>
+function myFunction() {
+  document.getElementById("demo1").innerHTML = "Hello Dolly!";
+  document.getElementById("demo2").innerHTML = "How are you?";
+}
+</script>
+
+using brackles  in  javascript
+*/
+
+/* let person = "John Doe",
+carName = "Volvo",
+price = 200;
+document.getElementById("demo").innerHTML = carName;
+
+writing mutiple variable by using one let 
+*/
+
+// javascript old variable is var and new variables are let and const 
+
---
+++ quiz.js
@@ -0,0 +1,5 @@
+'hello'
+'hello'
+alert('hello')
+
+'some'+'text'
